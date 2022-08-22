5b3f21f6d7f2f9dbabb3e2bb32be893c77836ac2
Kamrul hasan <kamrulhasan275723@gmail.com>
2022-08-22 19:29:40 +0600

    calculation add

---
+++ calculation.js
@@ -0,0 +1,5 @@
+document.getElementById('calculate-btn').addEventListener('click',function(){
+    displaySelectedV(playerNameArray);
+    const selectedPlayerLength =  playerNameList.length;
+    console.log(selectedPlayerLength)
+})
--- script.js
+++ script.js
@@ -1,6 +1,45 @@
+const playerNameArray = [];
+
+function displaySelectedV(playerNameList){
+    const tableBody = document.getElementById('players');
+      
+    tableBody.innerHTML = "";
+    const selectedPlayerLength =  playerNameList.length;
+   
+    
+    for(let i = 0; i < selectedPlayerLength; i++){
+       const tr = document.createElement('tr');
+      
+       if(playerNameList.length < 6){
+       
+        tr.innerHTML = `
+        <th>${i + 1}</th>
+        <td>${playerNameList[i]}</td>
+        `
+        tableBody.appendChild(tr);
+      
+        
+        
+      }
+    
+      else{
+        
+        alert('Dont add')
+
+        return;
+
+     }  
+     
+    }     
+   
+   
+}
+
 function addToSelectedV(element){
 
     const playerName = element.parentNode.children[0].innerText;
-    console.log(playerName)
-
+    playerNameArray.push(playerName);
+    displaySelectedV(playerNameArray);
+    element.classList.add("disabled");
+   
 }
